0edb6b853e40ad3d93c33633f8e4c1d3a3e2bf6f
Sheldon Griffin <sheldon@chrisdavidgriffin@yahoo.com>
2020-11-23 22:55:06 -0600

    add w10 library from https://github.com/Disassembler0/Win10-Initial-Setup-Script call desired tweaks on install

--- tools/ChocolateyInstall.ps1
+++ tools/ChocolateyInstall.ps1
@@ -1,17 +1,20 @@
 Import-Module $PSScriptRoot\chocolatey.psm1
 Import-Module $PSScriptRoot\wsl.psm1
 Import-Module $PSScriptRoot\vstweaks.psm1
+Import-Module $PSScriptRoot\w10tweaks.psm1
 
 Update-ExecutionPolicy Unrestricted
 
 Disable-UAC
 Disable-MicrosoftUpdate
 
+# Is this being run? vssadmin Delete Shadows /For=$env:SYSTEMDRIVE /Quiet
 Disable-ComputerRestore "C:\", "D:\", "E:\"
 
 InstallChocoPackages
 InstallWSL2
 TweakCodeAndStudio
+TweakWin10
 
 Enable-UAC
 Enable-MicrosoftUpdate
